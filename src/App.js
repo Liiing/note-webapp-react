115f84f8b8d8ac7a457653cddf555ec8db29be71
Yeung <yiring.yeung@ger.win.int.kn>
2021-02-20 18:49:40 +0100

    uuid added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.scss';
 import React, { useState } from 'react';
 import NavBar from './NavBar';
 import NoteList from './NoteList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [noteList, setNotsList] = useState([
     {
+      uuid: uuidv4(),
       title: "title",
       content: "content",
     }
--- src/NoteList.js
+++ src/NoteList.js
@@ -2,7 +2,7 @@ import Note from './Note'
 
 function NoteList(props) {
     const renderedNoteList = props.noteList.map(note => (
-       <Note title={note.title} content={note.content}/>
+       <Note title={note.title} content={note.content} key={note.uuid}/>
     ))
 
     return (
