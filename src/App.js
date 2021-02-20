f5d0a091d720792e011d47e1a9cbedb6328435aa
Yeung <yiring.yeung@ger.win.int.kn>
2021-02-20 16:09:48 +0100

    extracted nav component into new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.scss';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-          </a>
-        </div>
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-end">
-            <a className="button is-primary">
-              <strong>+</strong>
-            </a>
-          </div>
-        </div>
-    </div>
-  </nav>
+    <NavBar/>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+function NavBar() {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+          </a>
+        </div>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <a className="button is-primary">
+              <strong>+</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
